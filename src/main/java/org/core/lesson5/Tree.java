6fd5c06f069c28a5313a0de7c0679bc1d2ad8839
Vadim <orc225@mail.ru>
2023-12-07 19:23:35 +0300

    second commit реализовано дерево

---
+++ src/main/java/org/core/lesson5/Tree.java
@@ -0,0 +1,50 @@
+package org.core.lesson5;
+
+import java.io.File;
+
+/**
+ * TODO: Доработать метод print, необходимо распечатывать директории и файлы
+ */
+public class Tree {
+
+    public static void main(String[] args) {
+        print(new File("."), "", true);
+    }
+
+    static void print(File file, String indent, boolean isLast) {
+        System.out.print(indent);
+        if (isLast) {
+            System.out.print("└─");
+            indent += "  ";
+        } else {
+            System.out.print("├─");
+            indent += "│ ";
+        }
+        System.out.println(file.getName());
+
+        File[] files = file.listFiles();
+        if (files == null)
+            return;
+
+        int subDirTotal = 0;
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].isDirectory())
+                subDirTotal++;
+        }
+        int subFilesTotal = 0;
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].isFile()) subFilesTotal++;
+        }
+
+
+        int subDirCounter = 0;
+        int subFilesCounter = 0;
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].isDirectory()) {
+                print(files[i], indent, subDirTotal == ++subDirCounter && subFilesTotal == subFilesCounter);
+            } else print(files[i], indent, subFilesTotal == ++subFilesCounter && subDirTotal == subDirCounter);
+        }
+    }
+}
+
+
